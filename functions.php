2fb624b1a4f160b4fea3fb9f25203e803299c2c5
Gibson <95596261+Gibsonmb71@users.noreply.github.com>
2026-06-17 18:27:56 -0400

    Prepare theme support for PR4 modules

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WEEKLY_WILDCAT_VERSION', '0.3.0' );
+define( 'WEEKLY_WILDCAT_VERSION', '0.4.0' );
 
 /**
  * Theme setup.
@@ -22,7 +22,8 @@ function weekly_wildcat_setup() {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'custom-logo' );
-	add_theme_support( 'html5', array( 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script', 'navigation-widgets' ) );
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'html5', array( 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script', 'navigation-widgets', 'search-form' ) );
 
 	add_editor_style( 'assets/css/editor.css' );
 }
@@ -66,6 +67,14 @@ function weekly_wildcat_register_pattern_categories() {
 		array(
 			'label'       => __( 'Weekly Wildcat Articles', 'weekly-wildcat' ),
 			'description' => __( 'Article endings, author modules, comments, and related-story patterns.', 'weekly-wildcat' ),
+		)
+	);
+
+	register_block_pattern_category(
+		'weekly-wildcat-modules',
+		array(
+			'label'       => __( 'Weekly Wildcat Modules', 'weekly-wildcat' ),
+			'description' => __( 'Reusable callouts, alerts, photo strips, and newsroom modules.', 'weekly-wildcat' ),
 		)
 	);
 }
